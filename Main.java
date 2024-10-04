b3fc713179d454509e011c551b3eaa0fbfc208f4
bronu31 <71917315+bronu31@users.noreply.github.com>
2024-10-04 16:20:36 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package org;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.LongSummaryStatistics;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        LostNumber lostNumber= new LostNumber();
+        Object[] arr=new Object[]{ 7.,9.,10., 11., 12.};
+        System.out.println(lostNumber.lostInteger(arr));
+        // массив может быть длинны от 0, но начиная с длинны 2 гарантировано будет пропущенный элемент
+        //последовательность не содержит отрицательных чисел
+        
+        
+    }
+    
+}
